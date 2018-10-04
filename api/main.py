9e77aae9d47d83671568a527c9450b995cd22a59
cesardramirez <cesardavid89@gmail.com>
2018-10-04 15:13:50 -0500

    feat(rank-remove): Obtiene las criptomonedas con ranking menores o iguales a 20. Elimina una o varias criptomonedas según el nombre.

--- api/main.py
+++ api/main.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 
 """
 jsonify: convierte diccionarios de python al formato json
-request: recibe la peticion de la url y los parametros (si tiene)
-
 """
 
 def get_db_connection(uri):
@@ -26,9 +24,11 @@ db_connection = get_db_connection('mongodb://localhost:27017/')
 def get_documents():
     """
     Obtiene todos los documentos de la coleccion de la BD.
-    :return: Una lista de los documentos según los parámetros definidos en el GET.
+    :return: Una lista de los documentos según el criterio de búsqueda.
     """
+
     params = {}
+    # request: Recibe la petición de la url y los parámetros (si tiene).
     name = request.args.get('name', '')  # Si no hay valor, será un str vacío.
     limit = int(request.args.get('limit', 0))
 
@@ -41,17 +41,39 @@ def get_documents():
     return list(cursor)
 
 
-def get_top20():
+def get_rank_top20():
     """
-    Obtiene los primeros 20 documentos.
-    :return:
+    Obtiene los documentos que tienen un ranking menor o igual a 20.
+    :return: Una lista de los documentos según el criterio de búsqueda.
     """
-    pass
+
+    params = {}
+    name = request.args.get('name', '')
+    limit = int(request.args.get('limit', 0))
+
+    if name:
+        params.update({'name': name})
+
+    params.update({'rank': {'$lte': 20}})
+
+    cursor = db_connection.tickers.find(params, {'_id': 0, 'ticker_hash': 0}).limit(limit)
+
+    return list(cursor)
 
 
 def remove_currency():
     """
-    Eliminar un documento de la coleccion.
-    :return:
+    Eliminar uno o varios documentos de la coleccion según el nombre de la criptomoneda.
+    :return: La cantidad de documentos eliminados.
     """
-    pass
+
+    params = {}
+    name = request.args.get('name', '')
+
+    if name:
+        params.update({'name': name})
+    else:
+        # El método sin parámetros eliminaría todos los documentos, por lo cuál no se debe permitir.
+        return False
+
+    return db_connection.tickers.delete_many(params).deleted_count
